0ca15a1816c68d683524009443eb684af0a86060
OwJao <j.antonioassuncao@outlook.com>
2025-09-04 01:21:33 -0300

    feat: add style to the Home Page and Axios setup

--- frontend/src/pages/Home/index.jsx
+++ frontend/src/pages/Home/index.jsx
@@ -1,6 +1,24 @@
 import "./style.css";
+import Trash from "../../assets/red-trash.svg";
 
 function Home() {
+    const users = [
+        {
+            id: 1,
+            nome: "João",
+            sobrenome: "Silva",
+            email: "joao@gmail.com",
+            idade: 30,
+        },
+        {
+            id: 2,
+            nome: "Maria",
+            sobrenome: "Oliveira",
+            email: "teste@g.com",
+            idade: 25,
+        },
+    ];
+
     return (
         <div className="container">
             <form>
@@ -8,21 +26,23 @@ function Home() {
                 <input type="text" placeholder="Nome" />
                 <input type="text" placeholder="Sobrenome" />
                 <input type="email" placeholder="Email" />
-                <input type="number" placeholder="Idade" />
+                <input type="number" max={100} min={1} placeholder="Idade" />
                 <button type="button">Cadastrar</button>
             </form>
 
-            <div className="users">
-                <div>
-                    <p>Nome: </p>
-                    <p>Sobrenome: </p>
-                    <p>Email: </p>
-                    <p>Idade: </p>
+            {users.map((user) => (
+                <div className="user" key={user.id}>
+                    <div>
+                        <p>Nome: {user.nome}</p>
+                        <p>Sobrenome: {user.sobrenome}</p>
+                        <p>Email: {user.email}</p>
+                        <p>Idade: {user.idade}</p>
+                    </div>
+                    <div>
+                        <img src={Trash} alt="Excluir" />
+                    </div>
                 </div>
-                <div>
-                    <img src="" alt="" />
-                </div>
-            </div>
+            ))}
         </div>
     );
 }
